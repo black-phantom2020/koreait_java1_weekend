b76392ac8e5d816b5267bd0114c3d2081d1aab3d
black-phantom2020 <chrimuelse@naver.com>
2021-02-20 14:46:50 +0900

    21.02.20. 자판기게임 풀이 끝

--- com.kita.first/src/com/kita/first/level2/practice/Practice11.java
+++ com.kita.first/src/com/kita/first/level2/practice/Practice11.java
@@ -27,9 +27,36 @@ public class Practice11 {
 //		합계: 3700원
 		
 		Scanner scan = new Scanner(System.in);
-		String[] menuArr = {};
-		int[] priceArr = {};
+		String[] menuArr = {"콜라", "사이다", "캔커피"
+							, "데자와", "환타", "웰치스", "솔의눈"};
+		int[] priceArr = {900, 700, 400, 500, 600, 1000, 800};
 		int num;
 		int total=0;
+		
+		System.out.println("--메뉴--");
+		for(int i=0; i<menuArr.length; i++) {
+			System.out.printf("%d. %s %d원\n", i+1
+						, menuArr[i], priceArr[i]);
+		}
+		System.out.println();
+		
+		while(true) {
+			System.out.print("메뉴를 입력하세요(0 입력 시 종료): ");
+			num = scan.nextInt();
+			
+			if(num < 0 || num > menuArr.length) {
+				System.out.println("범위 내의 값을 입력해주세요.");
+				continue;
+			} else if(num == 0) {
+				break;
+			}
+			
+			System.out.printf("%s %d원\n", menuArr[num-1]
+							, priceArr[num-1]);
+			total += priceArr[num-1];
+		}
+		
+//		System.out.println("합계: " + total + "원");
+		System.out.printf("합계: %d원\n", total);
 	}
 }
